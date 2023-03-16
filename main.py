9287e412ba86ba1ded65abae346da61b7ab89de4
Никита <GhostOfSummer@yandex.ru>
2023-03-16 16:57:36 +0300

    m gh

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template
+from flask_login import LoginManager, login_user
+from flask_wtf import FlaskForm
+from werkzeug.utils import redirect
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 from data import db_session
 from data.jobs import Jobs
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 @app.route('/member')
@@ -12,6 +32,27 @@ def sample_file_upload():
     db_sess = db_session.create_session()
     news = db_sess.query(Jobs)
     return render_template("auto_answer.html", news=news)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 if __name__ == '__main__':
